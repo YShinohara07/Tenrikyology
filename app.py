924e9911ffc04006e7f51577c8628175c0aa34d6
Yasuhiro Shinohara <yasuhiro.yks@gmail.com>
2020-07-14 16:21:36 -0400

    updates to app

--- app.py
+++ app.py
@@ -7,17 +7,29 @@ df = pd.read_csv('TenrikyologyContent.csv')
 font_style = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=font_style)
 
-def dropdown(column, df=df):
+def dropdown(column):
     '''Creates dropdown search using string column of dataframe'''
     option = [dict(label=i,value=i) for i in df[column].dropna().unique()]
     return html.Div([
         html.Label(column),
-        dcc.Dropdown(options=option, value=column)
+        dcc.Dropdown(id=column, options=option, value=column)
     ])
 
+def filter_dropdown(dropdown_value):
+    return df.loc[ df[df.columns[0]] == dropdown_value, df.columns[4] ].dropna().unique()
 
-app.layout = dropdown(df.columns[0])
+app.layout = html.Div([
+    html.H6('Tenrikyology Content Search'),
+    html.Hr(),
+    dropdown(df.columns[0]),
+    html.Br(),
+    ddc.Dropdown(id=df.columns[1])
+])
 
+@app.callback(
+    Output(component_id=df.columns[1], component_property='children'),
+    [Input(component_id=df.columns[0], component_property='value')]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
