5a8611753f66184551d6aecf507b6669a47803b5
Yasuhiro Shinohara <yasuhiro.yks@gmail.com>
2020-07-14 14:48:46 -0400

    created dropdown menu

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ df = pd.read_csv('TenrikyologyContent.csv')
 font_style = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=font_style)
 
+def dropdown(column, df=df):
+    '''Creates dropdown search using string column of dataframe'''
+    option = [dict(label=i,value=i) for i in df[column].dropna().unique()]
+    return html.Div([
+        html.Label(column),
+        dcc.Dropdown(options=option, value=column)
+    ])
+
+
+app.layout = dropdown(df.columns[0])
 
 
 if __name__ == '__main__':
