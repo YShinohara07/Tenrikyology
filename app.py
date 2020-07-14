43705c09d79d252f7a206b97ad2be819ad21a9c1
Yasuhiro Shinohara <yasuhiro.yks@gmail.com>
2020-07-14 12:27:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ import dash_html_components as html
 import pandas as pd
 
 df = pd.read_csv('TenrikyologyContent.csv')
+font_style = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=font_style)
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
